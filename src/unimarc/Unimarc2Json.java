64d6f2ff64023df6825dabd858e4aab6c9f9b2f2
iccu.andrea.giuliano <iccu.andrea.giuliano@gmail.com>
2017-10-31 16:58:38 +0100

    Migrazione a Gson
    - completata la migrazione. Inoltre sono esportati anche i null e gli
    array vuoti
    - rimosso completamente ogni riferimento alla libreria JsonSimple, più
    che valida, ma meno di Gson

--- src/unimarc/Unimarc2Json.java
+++ src/unimarc/Unimarc2Json.java
@@ -1,23 +1,4 @@
 package unimarc;
-/**
- * Copyright (C) 2002-2006 Bas Peters
- * 
- * This file is part of MARC4J
- * 
- * MARC4J is free software; you can redistribute it and/or modify it under the
- * terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- * 
- * MARC4J is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with MARC4J; if not, write to the Free Software Foundation, Inc., 59
- * Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -40,8 +21,6 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.SimpleLayout;
 import org.apache.log4j.WriterAppender;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.marc4j.MarcReader;
 import org.marc4j.MarcStreamReader;
 import org.marc4j.marc.ControlField;
@@ -51,13 +30,13 @@ import org.marc4j.marc.Subfield;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 
 /**
  * Esporta in un formato JSON dei record UNIMARC. Questi devono essere ISO2709,
- * con estensione ".mrc.gz"
+ * con estensione ".mrc.gz", tutti in una stessa directory
  */
 public class Unimarc2Json
 {
@@ -97,8 +76,8 @@ public class Unimarc2Json
 
 	public static void main(String args[]) throws Exception
 	{
-		Unimarc2Json ex = new Unimarc2Json();
-		File[] files = ex.fileArray(new File(args[0]));
+		Unimarc2Json uj = new Unimarc2Json();
+		File[] files = uj.fileArray(new File(args[0]));
 		Arrays.sort(files);
 		InputStream input = null;
 		Writer output = null;
@@ -136,21 +115,14 @@ public class Unimarc2Json
 				output = new OutputStreamWriter(new FileOutputStream(noext + ".json"), "ISO-8859-1");
 			}
 
-			// JsonWriter jw = new JsonWriter(new OutputStreamWriter(System.err));
-			// jw.setHtmlSafe(true);
-			// jw.setIndent("  ");
-
 			MarcReader reader = new MarcStreamReader(input);
-			JSONArray jRecords = new JSONArray();
-			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-			JsonParser jp = new JsonParser();
-			JsonElement je = null;
+			Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().serializeNulls().create();
+//			Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 			output.write("[\n");
 			while(reader.hasNext())
 			{
 				Record record = reader.next();
 				String bid = null;
-				JSONObject jRecord = new JSONObject();
 				JsonObject gRecord = new JsonObject();
 
 				char status = record.getLeader().toString().charAt(5);
@@ -161,15 +133,12 @@ public class Unimarc2Json
 				switch(level)
 				{
 					case 'm':
-						jRecord.put("livello", "monografia");
 						gRecord.addProperty("livello", "monografia");
 						break;
 					case 'a':
-						jRecord.put("livello", "spoglio");
 						gRecord.addProperty("livello", "spoglio");
 						break;
 					case 's':
-						jRecord.put("livello", "periodico");
 						gRecord.addProperty("livello", "periodico");
 						break;
 					default:
@@ -180,55 +149,42 @@ public class Unimarc2Json
 				switch(tipo)
 				{
 					case 'a':
-						jRecord.put("tipo", "Testo a stampa");
 						gRecord.addProperty("tipo", "Testo a stampa");
 						break;
 					case 'b':
-						jRecord.put("tipo", "Testo manoscritto");
 						gRecord.addProperty("tipo", "Testo manoscritto");
 						break;
 					case 'c':
-						jRecord.put("tipo", "Musica a stampa");
 						gRecord.addProperty("tipo", "Musica a stampa");
 						break;
 					case 'd':
-						jRecord.put("tipo", "Musica manoscritta");
 						gRecord.addProperty("tipo", "Musica manoscritta");
 						break;
 					case 'e':
-						jRecord.put("tipo", "Cartografia a stampa");
 						gRecord.addProperty("tipo", "Cartografia a stampa");
 						break;
 					case 'f':
-						jRecord.put("tipo", "Cartografia manoscritta");
 						gRecord.addProperty("tipo", "Cartografia manoscritta");
 						break;
 					case 'g':
-						jRecord.put("tipo", "Materiale video");
 						gRecord.addProperty("tipo", "Materiale video");
 						break;
 					case 'k':
-						jRecord.put("tipo", "Materiale grafico");
 						gRecord.addProperty("tipo", "Materiale grafico");
 						break;
 					case 'i':
-						jRecord.put("tipo", "Registrazione sonora non musicale");
 						gRecord.addProperty("tipo", "Registrazione sonora non musicale");
 						break;
 					case 'j':
-						jRecord.put("tipo", "Registrazione sonora musicale");
 						gRecord.addProperty("tipo", "Registrazione sonora musicale");
 						break;
 					case 'l':
-						jRecord.put("tipo", "Risorsa elettronica");
 						gRecord.addProperty("tipo", "Risorsa elettronica");
 						break;
 					case 'm':
-						jRecord.put("tipo", "Materiale multimediale");
 						gRecord.addProperty("tipo", "Materiale multimediale");
 						break;
 					case 'r':
-						jRecord.put("tipo", "Oggetto multimediale");
 						gRecord.addProperty("tipo", "Oggetto multimediale");
 						break;
 					default:
@@ -249,35 +205,22 @@ public class Unimarc2Json
 					switch(tag)
 					{
 						case "001":
-							// bid = field.getData().substring(8, 11);
-							// bid += "\\" + field.getData().substring(12);
 							bid = field.getData();
 							log.info("BID: " + bid);
-							jRecord.put("codiceIdentificativo", bid);
-							// jw.beginObject();
-							// jw.name("codiceIdentificativo").value(bid);
-							// jw.endObject();
+							gRecord.addProperty("codiceIdentificativo", bid);
 							break;
 						default:
 							break;
 					}
 				}
-				// if(!bid.contains("BVE") &&
-// (bid.compareToIgnoreCase("IT\\ICCU\\BVE\\0655516")
-				// < 0))
-				// if(bid.compareToIgnoreCase("IT\\ICCU\\BVE\\0655516") < 0)
-				// if(bid.compareToIgnoreCase("IT\\ICCU\\BVE\\0037416") < 0)
-				// {
-				// continue;
-				// }
-				JSONArray jLocs = new JSONArray();
-				JSONArray jNomi = new JSONArray();
-				JSONArray jCDDs = new JSONArray();
-				JSONArray jNumeri = new JSONArray();
-				JSONArray jSoggetti = new JSONArray();
-				JSONArray jVarianti = new JSONArray();
-				JSONObject jNumero = null;
-				JSONObject jCDD = null;
+				JsonArray gLocs = new JsonArray();
+				JsonArray gNomi = new JsonArray();
+				JsonArray gCDDs = new JsonArray();
+				JsonArray gNumeri = new JsonArray();
+				JsonArray gSoggetti = new JsonArray();
+				JsonArray gVarianti = new JsonArray();
+				JsonObject gNumero = null;
+				JsonObject gCDD = null;
 				while(dfIter.hasNext())
 				{
 					DataField field = (DataField) dfIter.next();
@@ -294,9 +237,9 @@ public class Unimarc2Json
 							{
 								data = field.getSubfield('z').getData() + " (" + field.getSubfield('b').getData() + ")";
 							}
-							jNumero = new JSONObject();
-							jNumero.put("ISBN", data);
-							jNumeri.add(jNumero);
+							gNumero = new JsonObject();
+							gNumero.addProperty("ISBN", data);
+							gNumeri.add(gNumero);
 							break;
 						case "011":
 							if(field.getSubfield('a') != null)
@@ -315,9 +258,9 @@ public class Unimarc2Json
 									log.warn("011$z mancante");
 								}
 							}
-							jNumero = new JSONObject();
-							jNumero.put("ISSN", data);
-							jNumeri.add(jNumero);
+							gNumero = new JsonObject();
+							gNumero.addProperty("ISSN", data);
+							gNumeri.add(gNumero);
 							break;
 						case "100":
 							data = field.getSubfield('a').getData();
@@ -325,11 +268,11 @@ public class Unimarc2Json
 							break;
 						case "101":
 							data = field.getSubfield('a').getData();
-							jRecord.put("linguaPubblicazione", data);
+							gRecord.addProperty("linguaPubblicazione", data);
 							break;
 						case "102":
 							data = field.getSubfield('a').getData();
-							jRecord.put("paesePubblicazione", data);
+							gRecord.addProperty("paesePubblicazione", data);
 							break;
 						case "200":
 							if(field.getSubfield('a') != null)
@@ -350,7 +293,7 @@ public class Unimarc2Json
 							{
 								data += " / " + field.getSubfield('f').getData();
 							}
-							jRecord.put("titolo", data);
+							gRecord.addProperty("titolo", data);
 							log.debug("titolo: " + data);
 							break;
 						case "210":
@@ -368,7 +311,7 @@ public class Unimarc2Json
 							{
 								data += ", " + field.getSubfield('d').getData();
 							}
-							jRecord.put("pubblicazione", data);
+							gRecord.addProperty("pubblicazione", data);
 							break;
 						case "215":
 							data = "";
@@ -380,7 +323,7 @@ public class Unimarc2Json
 							{
 								data += " ; " + field.getSubfield('d').getData();
 							}
-							jRecord.put("descrizioneFisica", data);
+							gRecord.addProperty("descrizioneFisica", data);
 							break;
 						case "410":
 							data = "";
@@ -393,7 +336,7 @@ public class Unimarc2Json
 								data += " ; " + field.getSubfield('v').getData();
 							}
 							data = clean(data);
-							jRecord.put("collezione", data);
+							gRecord.addProperty("collezione", data);
 							break;
 						case "461":
 							if(field.getSubfield('a') != null)
@@ -414,7 +357,7 @@ public class Unimarc2Json
 							{
 								data += " / " + field.getSubfield('f').getData();
 							}
-							jRecord.put("contenutoIn", data);
+							gRecord.addProperty("contenutoIn", data);
 							break;
 						case "517":
 							if(field.getSubfield('a') != null)
@@ -426,7 +369,7 @@ public class Unimarc2Json
 							{
 								log.warn(bid + ": privo di titolo");
 							}
-							jVarianti.add(data);
+							gVarianti.add(new JsonPrimitive(data));
 							break;
 						case "606":
 							if(field.getSubfield('a') != null)
@@ -435,7 +378,7 @@ public class Unimarc2Json
 							}
 							if(field.getSubfields('x') != null)
 							{
-								Iterator iter = field.getSubfields('x').iterator();
+								Iterator<Subfield> iter = field.getSubfields('x').iterator();
 								if(iter.hasNext())
 								{
 									data += " - " + ((Subfield) iter.next()).getData();
@@ -445,21 +388,21 @@ public class Unimarc2Json
 									}
 								}
 							}
-							jSoggetti.add(data);
+							gSoggetti.add(new JsonPrimitive(data));
 							// jRecord.put("soggetti", data);
 							break;
 						case "676":
 							if(field.getSubfield('a') != null)
 							{
 								data = field.getSubfield('a').getData();
-								jCDD = new JSONObject();
-								jCDD.put("cdd", data);
+								gCDD = new JsonObject();
+								gCDD.addProperty("cdd", data);
 							}
 							data = field.getSubfield('c').getData();
-							jCDD.put("dec", data);
+							gCDD.addProperty("dec", data);
 							data = field.getSubfield('v').getData();
-							jCDD.put("ed", data);
-							jCDDs.add(jCDD);
+							gCDD.addProperty("ed", data);
+							gCDDs.add(gCDD);
 							break;
 						case "700":
 							data = field.getSubfield('a').getData();
@@ -467,8 +410,8 @@ public class Unimarc2Json
 							{
 								data += field.getSubfield('b').getData();
 							}
-							jNomi.add(data);
-							jRecord.put("autorePrincipale", data);
+							gNomi.add(new JsonPrimitive(data));
+							gRecord.addProperty("autorePrincipale", data);
 							break;
 						case "701":
 							data = field.getSubfield('a').getData();
@@ -476,7 +419,7 @@ public class Unimarc2Json
 							{
 								data += field.getSubfield('b').getData();
 							}
-							jNomi.add(data);
+							gNomi.add(new JsonPrimitive(data));
 							break;
 						case "711":
 							data = field.getSubfield('a').getData();
@@ -484,7 +427,7 @@ public class Unimarc2Json
 							{
 								data += field.getSubfield('b').getData();
 							}
-							jNomi.add(data);
+							gNomi.add(new JsonPrimitive(data));
 							break;
 						case "710":
 							data = field.getSubfield('a').getData();
@@ -492,7 +435,7 @@ public class Unimarc2Json
 							{
 								data += field.getSubfield('b').getData();
 							}
-							jRecord.put("autorePrincipale", data);
+							gRecord.addProperty("autorePrincipale", data);
 							break;
 						case "702":
 							data = field.getSubfield('a').getData();
@@ -500,24 +443,24 @@ public class Unimarc2Json
 							{
 								data += field.getSubfield('b').getData();
 							}
-							jNomi.add(data);
+							gNomi.add(new JsonPrimitive(data));
 							break;
 						case "712":
 							data = field.getSubfield('a').getData();
 							if(field.getSubfield('b') != null)
 							{
-								data += field.getSubfield('b').getData();
+								data += field.getSubfield('b').getData();                                                                                                                                                                                                                                                                                                                                                                                                                            
 							}
-							jNomi.add(data);
+							gNomi.add(new JsonPrimitive(data));
 							break;
 						case "899":
 							Subfield s1 = field.getSubfield('1');
 							if(s1 != null)
 							{
 								data = s1.getData();
-								JSONObject jLoc = new JSONObject();
-								jLoc.put("isil", data);
-								jLocs.add(jLoc);
+								JsonObject gLoc = new JsonObject();								
+								gLoc.addProperty("isil", data);
+								gLocs.add(gLoc);
 							}
 							break;
 						default:
@@ -526,28 +469,22 @@ public class Unimarc2Json
 				}
 				if(!bid.contains("ZZZ"))
 				{
-					jRecord.put("localizzazioni", jLocs);
-					if(jNomi.size() > 0) jRecord.put("nomi", jNomi);
-					if(jNumeri.size() > 0) jRecord.put("numeri", jNumeri);
-					if(jCDDs.size() > 0) jRecord.put("dewey", jCDDs);
-					if(jSoggetti.size() > 0) jRecord.put("soggetti", jSoggetti);
-					if(jVarianti.size() > 0) jRecord.put("altriTitoli", jVarianti);
-					je = jp.parse(jRecord.toString());
-					String prettyJsonString = gson.toJson(je);
-					output.write(prettyJsonString);
+					gRecord.add("nomi", gNomi);
+					gRecord.add("numeri", gNumeri);
+					gRecord.add("dewey", gCDDs);
+					gRecord.add("soggetti", gSoggetti);
+					gRecord.add("altriTitoli", gVarianti);
+					String prettyGsonString = gson.toJson(gRecord);
+					output.write(prettyGsonString);
+					if(reader.hasNext())
+					{
+						output.write(",\n");
+					}
 					output.flush();
 				}
 				else
 				{
 					log.warn("record tipo ZZZ, scartato");
-				}
-// if(count >= max)
-// break;
-// else
-// output.write(",\n");
-				if(reader.hasNext())
-				{
-					output.write(",\n");
 				}
 			}
 			output.write("\n]");
@@ -557,19 +494,5 @@ public class Unimarc2Json
 			log.removeAllAppenders();
 			input.close();
 		}
-// jw.endArray();
-// jw.close();
-// output.write(jRecords.toString());
-// jRecords.writeJSONString(output);
-// Gson gson = new
-// GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-// JsonParser jp = new JsonParser();
-// JsonElement je = jp.parse(jRecords.toString());
-// String prettyJsonString = gson.toJson(je);
-// output.write(prettyJsonString);
-// output.write(jRecords.toString());
-// Properties prop = new Properties();
-// prop.put("nome", "andrea");
-// System.err.println(gson.toJson(prop));
 	}
 }
