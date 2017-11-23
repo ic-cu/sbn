9c6325ac5bd6932d2607078baab7de9e99e0edd5
iccu.andrea.giuliano <iccu.andrea.giuliano@gmail.com>
2017-11-23 12:00:05 +0100

    Legami con data e controllo guida area4
    - la query per titoli riporta anche la data del legame con il luogo ed
    estrae solo quelli con legame con data successiva ad un parametro
    configurato nel file di prop
    - in caso di guida ISBD errata (indice stringa out of bound) lancia
    l'eccezione ma prosegue dopo un messaggio di errore

--- src/indice/LuoghiTitoliArea4.java
+++ src/indice/LuoghiTitoliArea4.java
@@ -16,7 +16,8 @@ public class LuoghiTitoliArea4
 {
 	private DB db;
 	private String qLuoghi;
-	private PreparedStatement isbdStmt;
+	private String dataDa;
+	private PreparedStatement isbdStmt, luoghiStmt;
 	private Properties conf;
 	private PrintWriter out;
 
@@ -33,9 +34,11 @@ public class LuoghiTitoliArea4
 			String url = conf.getProperty("db.url");
 			String user = conf.getProperty("db.user");
 			String pass = conf.getProperty("db.pass");
+			dataDa = conf.getProperty("param.datada");
 			db = new DB(driver, url, user, pass);
 			qLuoghi = conf.getProperty("query.luoghi");
 			Log.info("Query luoghi: " + qLuoghi);
+			luoghiStmt = db.prepare(conf.getProperty("query.luoghi"));
 			isbdStmt = db.prepare(conf.getProperty("query.isbd"));
 			out = new PrintWriter(conf.getProperty("output.file"));
 		}
@@ -54,54 +57,75 @@ public class LuoghiTitoliArea4
 		LuoghiTitoliArea4 lt = new LuoghiTitoliArea4();
 		try
 		{
-			ResultSet luoghi = lt.db.select(lt.qLuoghi);
+// lt.luoghiStmt.setDate(1, java.sql.Date.valueOf(lt.dataDa));
+			ResultSet luoghi = lt.luoghiStmt.executeQuery();
 			while(luoghi.next())
 			{
 				String lid = luoghi.getString("LID");
 				String luogo = luoghi.getString("DS_LUOGO");
 				String legami = luoghi.getString("legami");
-				Log.info("LID: " + lid + ", Luogo: " + luogo + " (" + legami + " legami)");
-				lt.out.println(lid + ", Luogo: " + luogo + " (" + legami + " legami)");
-				lt.isbdStmt.setString(1, lid);
+				lt.isbdStmt.setDate(1, java.sql.Date.valueOf(lt.dataDa));
+				lt.isbdStmt.setString(2, lid);
 				ResultSet isbdRS = lt.isbdStmt.executeQuery();
+				if(isbdRS.isBeforeFirst())
+				{
+					Log.info("LID: " + lid + ", Luogo: " + luogo + " (" + legami + " legami)");
+					lt.out.println(lid + ", Luogo: " + luogo + " (" + legami + " legami)");
+				}
+				boolean found = false;
 				while(isbdRS.next())
 				{
+					found = true;
 					String bid = isbdRS.getString("BID");
 					String isbd = isbdRS.getString("ISBD");
 					String indice = isbdRS.getString("INDICE_ISBD");
-					Log.debug("BID: " + bid + ", ISBD: " + isbd + ", Indice ISBD " + indice);
-					String area4 = "";
-					int i1, i2, i210 = 0, f210 = 0;
-					i1 = indice.indexOf("210-");
-					if(i1 > 0)
+					String dataVar = isbdRS.getString("TS_VAR").substring(0, 10);
+					try
 					{
-						i1 += 4;
-						i2 = indice.indexOf(";", i1);
-						i210 = Integer.valueOf(indice.substring(i1, i2));
-//						f210 = i2;
-						i1 = indice.indexOf("-", i2) + 1;
+						Log.debug("BID: " + bid + ", ISBD: " + isbd + ", Indice ISBD " + indice);
+						String area4 = "";
+						int i1, i2, i210 = 0, f210 = 0;
+						i1 = indice.indexOf("210-");
 						if(i1 > 0)
 						{
+							i1 += 4;
 							i2 = indice.indexOf(";", i1);
-							f210 = Integer.valueOf(indice.substring(i1, i2));
-							area4 = isbd.substring(i210 - 1, f210 - 5);
+							i210 = Integer.valueOf(indice.substring(i1, i2));
+							i1 = indice.indexOf("-", i2) + 1;
+							if(i1 > 0)
+							{
+								i2 = indice.indexOf(";", i1);
+								f210 = Integer.valueOf(indice.substring(i1, i2));
+								area4 = isbd.substring(i210 - 1, f210 - 5);
+							}
+							else
+							{
+								area4 = isbd.substring(i210 - 1);
+							}
 						}
-						else
-						{
-							area4 = isbd.substring(i210 - 1);
-						}
+						Log.debug("Indice 210: " + i210 + ", " + f210 + ", indice: " + indice);
+						Log.info("Area 4: [" + area4 + "] isbd: " + isbd + ", indice: " + indice);
+						lt.out.println(bid + "(" + dataVar + "), area4: " + area4);
 					}
-					Log.debug("Indice 210: " + i210 + ", " + f210 + ", indice: " + indice);
-					Log.info("Area 4: [" + area4 + "] isbd: " + isbd + ", indice: " + indice);
-					lt.out.println(bid + ", area4: " + area4);
+					catch(StringIndexOutOfBoundsException e)
+					{
+						Log.warn(bid + ", guida area ISBD errata");
+					}
 				}
-				lt.out.println();
-				lt.out.println();
+				if(found)
+				{
+					lt.out.println();
+					lt.out.println();
+				}
 				lt.out.flush();
 			}
 			lt.out.close();
 		}
 		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		catch(StringIndexOutOfBoundsException e)
 		{
 			e.printStackTrace();
 		}
